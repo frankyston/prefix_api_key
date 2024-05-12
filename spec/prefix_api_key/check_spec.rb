00fe2758d4f676a7ec20d3e65802c7b66a95c986
Frankyston Lins <frankyston@gmail.com>
2024-05-12 09:16:09 -0300

    adjustment prefix

--- spec/prefix_api_key/check_spec.rb
+++ spec/prefix_api_key/check_spec.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-RSpec.describe PrefixApiKey::Check do
+RSpec.describe 'PrefixApiKey::Check' do
   context 'Check token' do
     it 'Should return true after check token' do
       set_configuration
-      result = PrefixApiKey::Generate.call(prefix: 'my-company')
-      check = PrefixApiKey::Check.call(token: result[:token], hash: result[:long_token_hash])
+      result = ::PrefixApiKey::Generate.call(prefix: 'my-company')
+      check = ::PrefixApiKey::Check.call(token: result[:token], hash: result[:long_token_hash])
       expect(check).to eq(true)
     end
 
     it 'Should return false after check token' do
       set_configuration
-      result = PrefixApiKey::Generate.call(prefix: 'my-company')
-      check = PrefixApiKey::Check.call(token: result[:token], hash: "#{result[:long_token_hash]}123")
+      result = ::PrefixApiKey::Generate.call(prefix: 'my-company')
+      check = ::PrefixApiKey::Check.call(token: result[:token], hash: "#{result[:long_token_hash]}123")
       expect(check).to eq(false)
     end
   end
--- spec/prefix_api_key/generate_spec.rb
+++ spec/prefix_api_key/generate_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-RSpec.describe PrefixApiKey::Generate do
+RSpec.describe 'PrefixApiKey::Generate' do
   it 'Should generate a token with prefix' do
     set_configuration
-    result = PrefixApiKey::Generate.call(prefix: 'prefix')
+    result = ::PrefixApiKey::Generate.call(prefix: 'prefix')
     expect(result[:prefix]).to eq('prefix')
     expect(result[:short_token].length).to eq(7)
   end
 
   it 'Should generate a token with company_test' do
     set_configuration
-    result = PrefixApiKey::Generate.call(prefix: 'company_test')
+    result = ::PrefixApiKey::Generate.call(prefix: 'company_test')
     expect(result[:prefix]).to eq('company_test')
   end
 end
