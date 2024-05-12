6c11efde2f71c67a2da52cd00156fdd34650cff6
Italo Matos <italomatos@gmail.com>
2024-05-11 22:07:36 -0300

    Ignore .rspec_status in future commits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 require 'prefix_api_key'
-require 'dotenv'
 require 'webmock'
 require 'pry'
-
-Dotenv.load('.env.test')
-Dotenv.load('.env.test.local')
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -22,9 +18,9 @@ end
 
 def set_configuration
   PrefixApiKey.configure do |config|
-    config.salt_short = ENV['SALT_SHORT']
-    config.salt_long = ENV['SALT_LONG']
-    config.password_short = ENV['PASSWORD_SHORT']
-    config.password_long = ENV['PASSWORD_LONG']
+    config.salt_short = 'salt_short'
+    config.salt_long = 'salt_long'
+    config.password_short = 'password_short'
+    config.password_long = 'password_long'
   end
 end
